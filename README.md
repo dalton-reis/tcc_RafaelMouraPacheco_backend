a17e1bef2a3d4eea6e5f20fcdbd9d5b666bc8baf
Rafael Pacheco <rafael.pacheco@senior.com.br>
2018-05-05 11:02:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Tagarela in NodeJS
+
+## Main features
+
+* User authentication
+
+## Prerequisites
+
+* [Node](https://nodejs.org) >= 6
+* [MySQL](https://www.mysql.com/)
+
+## How to run
+
+### Database
+
+1. Create database schema running the file `database.sql`
+
+### Backend
+
+1. `npm install`
+1. `npm install -g gulp yarn`
+1. `npm run build`
+1. `npm start`
+1. `npm run watch` for watch source changes
+
+Runs on localhost:3000
+
+Default system user/password: admin1/senha
---
+++ src/app.ts
@@ -0,0 +1,95 @@
+import * as express from "express";
+import * as logger from "morgan";
+import * as bodyParser from "body-parser";
+import LoginRouter from "./routes/login-router";
+import TokenRouter from "./routes/token-router";
+let jwt = require('jsonwebtoken');
+
+class App {
+
+    public express: express.Application;
+
+    constructor() {
+        this.express = express();
+        this.middleware();
+        this.routes();
+    }
+
+    private middleware(): void {
+        this.express.use(logger('dev'));
+        this.express.use(bodyParser.json());
+        this.express.use(bodyParser.urlencoded({extended: false}));
+    }
+
+    private routes(): void {
+        let router = express.Router();
+
+        router.get('/', (req, res, next) => {
+            res.json({
+                message: 'Hello World!'
+            });
+        });
+
+        let corsMiddleware = express.Router();
+
+        corsMiddleware.use(function (req, res, next) {
+
+            // Website you wish to allow to connect
+            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+            // Request methods you wish to allow
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+            // Request headers you wish to allow
+            res.setHeader('Access-Control-Allow-Headers',
+                'Access-Control-Allow-Methods,' +
+                'Authorization,' +
+                'Access-Control-Allow-Headers,' +
+                'Access-Control-Allow-Origin,' +
+                'X-Requested-With,' +
+                'content-type,' +
+                'X-Auth-Token');
+
+            if ('OPTIONS' == req.method) {
+                res.send(200);
+            } else {
+                next();
+            }
+
+            // Set to true if you need the website to include cookies in the requests sent
+            // to the API (e.g. in case you use sessions)
+            // res.setHeader('Access-Control-Allow-Credentials', true);
+
+            // Pass to next layer of middleware
+            // next();
+        });
+
+        let authMiddleware = express.Router();
+
+        authMiddleware.use((req, res, next) => {
+            let token: string = req.headers['authorization'];
+            if (token) {
+                jwt.verify(token, 'ITATAKARU', function (err, decoded) {
+                    if (err) {
+                        return res.status(401).send({
+                            message: 'Invalid token.'
+                        });
+                    } else {
+                        next();
+                    }
+                })
+            } else {
+                return res.status(403).send({
+                    message: 'No token provided.'
+                })
+            }
+        });
+
+        this.express.use('/', corsMiddleware, router);
+        this.express.use('/api/login', corsMiddleware, LoginRouter);
+        this.express.use('/api/token', corsMiddleware, TokenRouter);
+    }
+
+}
+
+export default new App().express;
